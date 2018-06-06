60ff3e63656dd5333f453aaa8039a8838e9de3a2
kouestu_serizawa <kouetsu_serizawa@mediado.jp>
2018-06-06 20:41:49 +0900

    genius!

--- public/js/Actions/GameListActions.js
+++ public/js/Actions/GameListActions.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 const GameListActions = {
     async getGameList(searchConditions) {
         const pathToAPI = APIPath.SEARCH;
-
         try {
             const result = await axios.get(pathToAPI + '?id=' + searchConditions.id + '&name=' + searchConditions.name + '&minPlayNum=' + searchConditions.minPlayNum + '&maxPlayNum=' + searchConditions.maxPlayNum);
             return result.data;
@@ -13,7 +12,18 @@ const GameListActions = {
         catch (e) {
             console.log(e);
             console.log('failed');
-            return [];
+            return;
+        }
+    },
+
+    async addBoardGame(postData) {
+        try{
+            const pathToAPI = APIPath.ADD;
+            const response = await axios.post(pathToAPI, postData);
+            return response;
+        }
+        catch(e){
+            console.log(e);
         }
     }
 }
