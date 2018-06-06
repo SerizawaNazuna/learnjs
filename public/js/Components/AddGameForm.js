6ec127e64564f75c0be91eb88cae5ce3997e632b
SerizawaNazuna <k.capricorn.s@gmail.com>
2018-06-07 01:42:57 +0900

    ねる

---
+++ public/js/Components/AddGameForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import Form from 'react-jsonschema-form';
+
+import GameListActions from '../Actions/GameListActions';
+import FormSchema from '../Consts/AddGameFormSchema';
+import uiSchema from '../Consts/AddGameUISchema';
+
--- public/js/Consts/SearchGameUISchema.js
+++ public/js/Consts/SearchGameUISchema.js
@@ -1,17 +1,14 @@
 const uiSchema = {
-    "name": {
-      "ui:autofocus": true,
-      "ui:emptyValue": ""
-    },
-    "playNumMin": {
-      "ui:widget": "updown"
-    },
-    "playNumMax": {
-      "ui:widget": "updown"
-    },
-    "bio": {
-      "ui:widget": "textarea"
-    }
+  "name": {
+    "ui:autofocus": true,
+    "ui:emptyValue": ""
+  },
+  "playNumMin": {
+    "ui:widget": "updown"
+  },
+  "playNumMax": {
+    "ui:widget": "updown"
   }
+}
 
-  export default uiSchema;
+export default uiSchema;
